8c2786ec011bf6d2b6fb087a30dcd169eeab0825
Pierre-Emmanuel41 <pierre.emmanuel41@gmail.com>
2021-09-06 01:38:05 +0200

    Using ArrayBlockingQueue in class Observable

--- src/main/java/fr/pederobien/utils/Observable.java
+++ src/main/java/fr/pederobien/utils/Observable.java
@@ -3,18 +3,17 @@ package fr.pederobien.utils;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import java.util.function.Consumer;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
 public class Observable<T> {
-	private List<T> observers, tempAddObserver, tempRemoveObserver;
-	private boolean isNotifying, addObserverFlag, removeObserverFlag;
+	private BlockingQueue<T> observers;
 
 	public Observable() {
-		observers = new ArrayList<T>();
-		tempAddObserver = new ArrayList<T>();
-		tempRemoveObserver = new ArrayList<T>();
+		observers = new ArrayBlockingQueue<T>(10000);
 	}
 
 	/**
@@ -23,12 +22,7 @@ public class Observable<T> {
 	 * @param obs The observer to add.
 	 */
 	public void addObserver(T obs) {
-		if (!isNotifying) {
-			observers.add(obs);
-			return;
-		}
-		addObserverFlag = true;
-		tempAddObserver.add(obs);
+		observers.add(obs);
 	}
 
 	/**
@@ -37,13 +31,7 @@ public class Observable<T> {
 	 * @param obs The observer to remove.
 	 */
 	public void removeObserver(T obs) {
-		if (!isNotifying) {
-			observers.remove(obs);
-			return;
-		}
-
-		removeObserverFlag = true;
-		tempRemoveObserver.add(obs);
+		observers.remove(obs);
 	}
 
 	/**
@@ -78,11 +66,10 @@ public class Observable<T> {
 	 * @return A list that contains all registered observers for this observable. This list is unmodifiable.
 	 */
 	public List<T> getObservers() {
-		return Collections.unmodifiableList(observers);
+		return Collections.unmodifiableList(new ArrayList<>(observers));
 	}
 
 	private void internalNotify(Stream<T> observers, Consumer<T> consumer) {
-		isNotifying = true;
 		observers.forEach(obj -> {
 			try {
 				consumer.accept(obj);
@@ -90,18 +77,5 @@ public class Observable<T> {
 				e.printStackTrace();
 			}
 		});
-
-		if (addObserverFlag) {
-			tempAddObserver.forEach(obs -> this.observers.add(obs));
-			tempAddObserver.clear();
-			addObserverFlag = false;
-		}
-		if (removeObserverFlag) {
-			tempRemoveObserver.forEach(obs -> this.observers.remove(obs));
-			tempRemoveObserver.clear();
-			removeObserverFlag = false;
-		}
-
-		isNotifying = false;
 	}
 }
