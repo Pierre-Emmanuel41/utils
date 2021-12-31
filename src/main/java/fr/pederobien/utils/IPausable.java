8a42b510222385a0400d3ae48fc9c974a2a84807
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2021-12-31 18:55:34 +0100

    Adding enum PausableState

--- src/main/java/fr/pederobien/utils/IPausable.java
+++ src/main/java/fr/pederobien/utils/IPausable.java
@@ -21,4 +21,26 @@ public interface IPausable {
 	 * Resumes the execution
 	 */
 	void resume();
+
+	/**
+	 * @return The state of this pausable object.
+	 */
+	PausableState getState();
+
+	public enum PausableState {
+		/**
+		 * When the object is instantiated but not started or has been stopped.
+		 */
+		NOT_STARTED,
+
+		/**
+		 * When the object is started, but neither paused nor stopped.
+		 */
+		STARTED,
+
+		/**
+		 * When the object is paused but not resumed.
+		 */
+		PAUSED
+	}
 }
