994245dc46700e56512248052a2c60e7008d8635
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2024-07-25 22:18:47 +0200

    Adding enum ElogLevel to LogEvent class

--- src/main/java/fr/pederobien/utils/event/LogEvent.java
+++ src/main/java/fr/pederobien/utils/event/LogEvent.java
@@ -1,18 +1,54 @@
 package fr.pederobien.utils.event;
 
-import java.util.StringJoiner;
-
 public class LogEvent extends Event {
+	
+	public enum ELogLevel {
+		INFO,
+		DEBUG,
+		WARNING,
+		ERROR
+	}
+	private ELogLevel level;
 	private String message;
 
-	public LogEvent(String format, Object... args) {
+	/**
+	 * Creates a log event.
+	 * 
+	 * @param level The level of the log.
+	 * @param format The formatter if the message to display has arguments.
+	 * @param args The arguments of the message to display.
+	 */
+	public LogEvent(ELogLevel level, String format, Object... args) {
+		this.level = level;
 		this.message = String.format(format, args);
+	}
+	
+	/**
+	 * Creates a log event. The log level is INFO.
+	 * 
+	 * @param format The formatter if the message to display has arguments.
+	 * @param args The arguments of the message to display.
+	 */
+	public LogEvent(String format, Object... args) {
+		this(ELogLevel.INFO, format, args);
+	}
+	
+	/**
+	 * @return The log level of this event.
+	 */
+	public ELogLevel getLevel() {
+		return level;
+	}
+
+	/**
+	 * @return The log message of the event.
+	 */
+	public String getMessage() {
+		return message;
 	}
 
 	@Override
 	public String toString() {
-		StringJoiner joiner = new StringJoiner(",", "{", "}");
-		joiner.add("message=" + message);
-		return String.format("%s_%s", getName(), joiner);
+		return String.format("[%s] %s", level, message);
 	}
 }
