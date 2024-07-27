d50987ba50101eea3f2468325d30be6fa9fd0597
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2024-07-27 17:11:55 +0200

    Adding interface IDisposable and its implementation

---
+++ src/main/java/fr/pederobien/utils/Disposable.java
@@ -0,0 +1,28 @@
+package fr.pederobien.utils;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Disposable implements IDisposable {
+	private AtomicBoolean isDisposed;
+	
+	public Disposable() {
+		isDisposed = new AtomicBoolean(false);
+	}
+
+	@Override
+	public boolean dispose() {
+		return isDisposed.compareAndSet(false, true);
+	}
+
+	@Override
+	public boolean isDisposed() {
+		return isDisposed.get();
+	}
+
+	@Override
+	public void checkDisposed() {
+		if (isDisposed())
+			throw new IllegalStateException("Object disposed");
+	}
+
+}
