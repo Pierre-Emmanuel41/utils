8cf55f01328e875c4e8a6fe3d7d7624acd893bf0
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2024-09-12 23:04:00 +0200

    Adding possibility to cancel task

--- src/main/java/fr/pederobien/utils/Watchdog.java
+++ src/main/java/fr/pederobien/utils/Watchdog.java
@@ -7,25 +7,38 @@ import java.util.concurrent.atomic.AtomicBoolean;
 public class Watchdog {
 	
 	/**
-	 * Execute asynchronously the runnable and block until the end of the execution or for a timeout.
+	 * Execute asynchronously the runnable and block until the end of the execution or until a timeout occurs.
 	 * 
 	 * @param executable The code to execute.
 	 * @param timeout The timeout in ms.
 	 * 
 	 * @return True if no timeout occurred, false otherwise.
-	 * @throws InterruptedException 
 	 */
-	public static boolean execute(IExecutable executable, int timeout) throws InterruptedException {
-		return new WatchdogStakeholder(executable, timeout).execute();
+	public static boolean start(IExecutable executable, int timeout) throws InterruptedException {
+		return new WatchdogStakeholder(executable, timeout).start();
 	}
 	
-	private static class WatchdogStakeholder {
+	/**
+	 * Create a watchdog stakeholder to execute asynchronously the runnable and
+	 * block until the end of the execution or for a timeout.
+	 * 
+	 * @param executable The code to execute.
+	 * @param timeout The timeout in ms.
+	 * 
+	 * @return The watchdog to execute the monitored task.
+	 */
+	public static WatchdogStakeholder create(IExecutable executable, int timeout) {
+		return new WatchdogStakeholder(executable, timeout);
+	}
+	
+	public static class WatchdogStakeholder {
 		private IExecutable executable;
 		private int timeout;
 		private Thread worker;
 		private Thread watcher;
 		private AtomicBoolean success;
 		private Semaphore out, monitor;
+		private boolean cancelled;
 		
 		/**
 		 * Creates a watchdog to execute code asynchronously within a limited time.
@@ -41,9 +54,29 @@ public class Watchdog {
 			success = new AtomicBoolean(false);
 			out = new Semaphore(0);
 			monitor = new Semaphore(1);
+			
+			cancelled = false;
 		}
 		
-		public boolean execute() throws InterruptedException {
+		/**
+		 * Cancel the execution of the task.
+		 */
+		public void cancel() {
+			cancelled = true;
+			
+			// Notifying the watcher thread
+			monitor.release();
+		}
+		
+		/**
+		 * Start the execution the of task in a separated thread and block until the end of it execution.
+		 * 
+		 * @return True if the task execution ended in time, false if a timeout occurred or if the task was cancelled.
+		 */
+		public boolean start() throws InterruptedException {
+			if (cancelled)
+				return false;
+
 			watcher.setDaemon(true);
 			watcher.start();
 			
@@ -76,13 +109,15 @@ public class Watchdog {
 				// Waiting a little bit to be sure the worker thread acquired the monitor semaphore
 				Thread.sleep(100);
 				
-				if (monitor.tryAcquire(timeout, TimeUnit.MILLISECONDS))
-					setSuccess(true);
-				else {
-					// Stopping worker thread execution
+				boolean inTime = monitor.tryAcquire(timeout, TimeUnit.MILLISECONDS);
+				
+				// When the execution is cancelled, inTime will be true as the monitor is released
+				if (cancelled || !inTime) {
 					worker.interrupt();
 					setSuccess(false);
 				}
+				else
+					setSuccess(true);
 			} catch (InterruptedException e) {
 				worker.interrupt();
 				setSuccess(false);
