1af1a5716681c8d39b27bb06b05c587fd4f51e02
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2025-02-01 11:36:37 +0100

    Improving watchdog

--- src/main/java/fr/pederobien/utils/Watchdog.java
+++ src/main/java/fr/pederobien/utils/Watchdog.java
@@ -1,11 +1,11 @@
 package fr.pederobien.utils;
 
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Semaphore;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Watchdog {
-	
+
 	/**
 	 * Execute asynchronously the runnable and block until the end of the execution or until a timeout occurs.
 	 * 
@@ -17,7 +17,7 @@ public class Watchdog {
 	public static boolean start(IExecutable executable, int timeout) throws InterruptedException {
 		return new WatchdogStakeholder(executable, timeout).start();
 	}
-	
+
 	/**
 	 * Create a watchdog stakeholder to execute asynchronously the runnable and
 	 * block until the end of the execution or for a timeout.
@@ -30,16 +30,16 @@ public class Watchdog {
 	public static WatchdogStakeholder create(IExecutable executable, int timeout) {
 		return new WatchdogStakeholder(executable, timeout);
 	}
-	
+
 	public static class WatchdogStakeholder {
 		private IExecutable executable;
 		private int timeout;
 		private Thread worker;
 		private Thread watcher;
-		private AtomicBoolean success;
 		private Semaphore out, monitor;
-		private boolean cancelled;
-		
+		private CountDownLatch countdownLatch;
+		private boolean cancelled, exception, success;
+
 		/**
 		 * Creates a watchdog to execute code asynchronously within a limited time.
 		 * 
@@ -49,85 +49,88 @@ public class Watchdog {
 		private WatchdogStakeholder(IExecutable executable, int timeout) {
 			this.executable = executable;
 			this.timeout = timeout;
-			
+
 			watcher = new Thread(() -> watch(), "Watcher");
-			success = new AtomicBoolean(false);
 			out = new Semaphore(0);
 			monitor = new Semaphore(1);
-			
+			countdownLatch = new CountDownLatch(2);
+
 			cancelled = false;
+			exception = false;
+			success = false;
 		}
-		
+
 		/**
 		 * Cancel the execution of the task.
 		 */
 		public void cancel() {
 			cancelled = true;
-			
+
 			// Notifying the watcher thread
 			monitor.release();
 		}
-		
+
 		/**
 		 * Start the execution the of task in a separated thread and block until the end of it execution.
 		 * 
 		 * @return True if the task execution ended in time, false if a timeout occurred or if the task was cancelled.
 		 */
 		public boolean start() throws InterruptedException {
-			if (cancelled)
-				return false;
+			cancelled = false;
+			exception = false;
 
 			watcher.setDaemon(true);
 			watcher.start();
-			
+
 			// Wait for the worker or the watcher
 			out.acquire();
-			return success.get();
+
+			return success;
 		}
-		
+
 		private void work() {
 			try {
 				// Acquiring permit to start timing monitoring
 				monitor.acquire();
-				
+
+				// Waiting a little bit to be sure the watcher thread is ready to monitor
+				countdownLatch.countDown();
+				countdownLatch.await();
+
 				// Execute the source code
 				executable.exec();
-				
+			} catch (Exception e) {
+				exception = true;
+			} finally {
 				// End of the execution
 				monitor.release();
-			} catch (Exception e) {
-				// Do nothing
 			}
 		}
-		
+
 		private void watch() {
+			boolean success = false;
 			try {
 				// Watcher is ready
 				worker = new Thread(() -> work(), "Worker");
 				worker.start();
-				
+
 				// Waiting a little bit to be sure the worker thread acquired the monitor semaphore
-				Thread.sleep(100);
-				
+				countdownLatch.countDown();
+				countdownLatch.await();
+
 				boolean inTime = monitor.tryAcquire(timeout, TimeUnit.MILLISECONDS);
-				
+
 				// When the execution is cancelled, inTime will be true as the monitor is released
-				if (cancelled || !inTime) {
-					worker.interrupt();
-					setSuccess(false);
-				}
-				else
-					setSuccess(true);
+				success = (cancelled || exception || !inTime) ? false : true;
+
 			} catch (InterruptedException e) {
-				worker.interrupt();
-				setSuccess(false);
-			} finally {
-				monitor.release();
+				success = false;
 			}
-		}
-		
-		private void setSuccess(boolean isSuccess) {
-			success.set(isSuccess);
+
+			this.success = success;
+			if (!success)
+				worker.interrupt();
+
 			out.release();
 		}
 	}
