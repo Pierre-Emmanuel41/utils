d2fe5ee3062850f18d291bbbf08a304a6fc10934
Pierre-Emmanuel <pierre.emmanuel41@gmail.com>
2026-08-15 13:32:49 +0200

    Deleting old event package

--- src/main/java/fr/pederobien/utils/event/EventRegistrationException.java
+++
@@ -1,9 +0,0 @@
-package fr.pederobien.utils.event;
-
-public class EventRegistrationException extends RuntimeException {
-	private static final long serialVersionUID = 1L;
-
-	public EventRegistrationException(String message) {
-		super(message);
-	}
-}
